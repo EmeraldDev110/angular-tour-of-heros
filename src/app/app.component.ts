c1331ee57e685891a61f53f7339fee1631a50628
Emerald Dev <emerald.dev@hotmail.com>
2024-07-30 19:04:05 -0700

    angular tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
